a62c00ddb8af8aa893a3d7d91bbd6252321b77d1
luisrvq <luisrvq@hotmail.com>
2020-02-25 08:06:45 -0500

    My Capstonr projrct notebook

---
+++ My Capstone Project lv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This NB will be used mainly for my Capstone Project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy  as np
+
+
+# In[4]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
